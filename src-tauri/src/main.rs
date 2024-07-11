720639efca5b3fa8cbf7c1cd1b41cd19cbe18d2a
Chetan Alla <allachetan@yahoo.com>
2024-07-11 18:38:49 -0400

    basic system tray ipml

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,8 +4,17 @@
 )]
 
 mod spotlight;
+use tauri::{SystemTray, CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem};
 
 fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+    let system_tray = SystemTray::new().with_menu(tray_menu);
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             spotlight::init_spotlight_window,
@@ -13,6 +22,7 @@ fn main() {
             spotlight::hide_spotlight
         ])
         .manage(spotlight::State::default())
+        .system_tray(system_tray)
         .setup(move |app| {
             // Set activation poicy to Accessory to prevent the app icon from showing on the dock
             app.set_activation_policy(tauri::ActivationPolicy::Accessory);
